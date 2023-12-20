98e73075794d4e4825837d5eb836e2d7a784c6c5
kouagoutiburce <kouagoutiburce@gmail.com>
2023-12-20 18:11:40 +0100

    création du HeaderItem.vue

---
+++ src/components/HeaderItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <li
+    class="flex flex-col items-center cursor-pointer w-20 sm:w-20 hover:text-white group">
+    <Icon class="h-8 mb-1  group-hover:animate-bounce" />
+    <a class="uppercase" href="#">{{ name }}</a>
+  </li>
+</template>
+
+<script setup lang="ts">
+import type { FunctionalComponent, HTMLAttributes, VNodeProps } from 'vue';
+
+
+const props = defineProps<{
+  name: String,
+  Icon: FunctionalComponent<HTMLAttributes & VNodeProps, {}, any>,
+}>();
+
+</script>
