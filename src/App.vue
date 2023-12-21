2e76bebf82571626cdcea8acdd527c5e7bf6df39
kouagoutiburce <kouagoutiburce@gmail.com>
2023-12-21 14:53:04 +0100

    NavBar créé

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <HeaderComp />
   <main>
-
+    <NavBar />
   </main>
 </template>
 
 <script setup lang="ts">
 import HeaderComp from "@/components/HeaderComp.vue";
+import NavBar from "./components/NavBar.vue";
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <ul
+      class="flex px-10 sm:px-20 text-2xl whitespace-nowrap gap-10 sm:gap-20 overflow-x-scroll  scrollbar-hide">
+      <li
+        class="transition duration-300 transform hover:scale-125 hover:text-white active:text-red-500"
+        v-for="link of requests" :key="link.title">
+        <button>{{ link.title }}</button>
+      </li>
+    </ul>
+  </nav>
+</template>
+<script setup lang="ts">
+import requests from "@/utils/request";
+
+const genreList: string[] = [];
+
+Object.entries(requests).forEach(([key, { title, url }]) => {
+  genreList.push(title);
+});
+</script>
